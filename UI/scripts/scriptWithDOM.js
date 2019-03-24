afdb32f3c8fcae9962a9f0f1f084b1157d4357cd
Kastsiukevich <kastsiukevich.g@gmail.com>
2019-03-24 15:38:21 +0300

    first version. only base functions and one class

--- UI/scripts/scriptWithDOM.js
+++ UI/scripts/scriptWithDOM.js
@@ -1,8 +1,17 @@
-class postsLent {
-
-    constructor() {
-        this.numOfPosts = JSON.parse(localStorage.getItem('posts_number'));
-        this.currentUser = JSON.parse(localStorage.getItem('current_user'));
+class postsView {
+    constructor(arr){
+        if(arr) {
+            this.currentUser = JSON.parse(localStorage.getItem('current_user'));
+            this.numOfPosts = 0;
+            for (let i = 0; i < arr.length; i++) {
+                if (this.validatePost(arr[i])) {
+                    this.addPost(arr[i]);
+                }
+            }
+        } else{
+            this.numOfPosts = JSON.parse(localStorage.getItem('posts_number'));
+            this.currentUser = JSON.parse(localStorage.getItem('current_user'));
+        }
     }
 
     sortPostsByDate(somePost) {
@@ -13,23 +22,6 @@ class postsLent {
                 return -1;
             }
         });
-    }
-
-    getPhotoPosts(skip = 0, top = 10, filterConfig) {
-        let res = photoPosts.slice();
-        this.sortPostsByDate(res);
-        if (filterConfig.author) {
-            res = res.filter(function (post) {
-                return post.author.toLowerCase() === filterConfig.author.toLowerCase();
-            });
-        }
-        if (filterConfig.createdAt) {
-            res = res.filter(function (post) {
-                return post.createdAt.getDate() === new Date(filterConfig.createdAt).getDate();
-            });
-        }
-        res = res.slice(skip, skip + top);
-        return res;
     }
 
     getPostById(idP) {
@@ -129,10 +121,13 @@ class postsLent {
     }
 
     newID(n) {
-        if (localStorage.getItem(n) === null) {
-            return n;
-        }
-        return n+'.1';
+        let tmp = (''+n).slice('');
+        let res = '';
+        for(let i = 0; i < 7 - tmp.length; i++)
+            res+='0';
+        for(let i = 0; i < tmp.length; i++)
+            res+=tmp[i];
+        return res;
     }
 
     addPost(post) {
@@ -142,27 +137,38 @@ class postsLent {
             post.id = this.newID(this.numOfPosts);
             document.getElementById('posts').innerHTML = this.createHTMLPost(post) + document.getElementById('posts').innerHTML;
             localStorage.setItem(post.id, JSON.stringify(post));
-            //photoPosts.push(post);
             return true;
         } else return false;
     }
 
-    editPost(postID, editConfig) {
-        let clone = this.getPostById(postID);
-        if (clone.author === this.currentUser) {
-            if (editConfig.id) clone.id = editConfig.id;
-            if (editConfig.description) clone.description = editConfig.description;
-            if (editConfig.createdAt) clone.createdAt = editConfig.createdAt;
-            if (editConfig.location) clone.location = editConfig.location;
-            if (editConfig.author) clone.author = editConfig.author;
-            if (editConfig.photoLink) clone.photoLink = editConfig.photoLink;
-            if (editConfig.likes) clone.photoLink = editConfig.photoLink;
-            if (this.validatePost(clone)) {
-                  localStorage.removeItem(postID);
-                  localStorage.setItem(clone.id, JSON.stringify(clone));
+    updateLent(){
+        document.getElementById('user_name').innerHTML = this.currentUser;
+        document.getElementById('posts').innerHTML = '';
+        let post;
+        for (let i = 1; i <= this.numOfPosts ; i++) {
+            post = JSON.parse(localStorage.getItem(this.newID(i)));
+            if (post.display !== false) {
+                document.getElementById('posts').innerHTML = this.createHTMLPost(post) + document.getElementById('posts').innerHTML;
             }
-        } else
-            return false;
+        }
+    }
+
+    editPost(id, config) {
+        let post = this.getPostById(id);
+        if (post && post.author === this.currentUser) {
+            localStorage.removeItem(id);
+            if (config.description) post.description = config.description;
+            if (config.createdAt) post.createdAt = config.createdAt;
+            if (config.location) post.location = config.location;
+            if (config.author) post.author = config.author;
+            if (config.photoLink) post.photoLink = config.photoLink;
+            if(this.validatePost(post)) {
+                localStorage.setItem(post.id, JSON.stringify(post));
+                this.updateLent();
+                return true;
+            }
+        }
+        return false;
     }
 
     removePost(id) {
@@ -174,47 +180,116 @@ class postsLent {
             localStorage.setItem( clone.id, JSON.stringify(clone));
             return true;
         }
-
         return false;
     }
 
-    
+}
 
+class headerView{
+    //ToDo
+}
+
+class filterView{
+    //ToDo
+}
+
+class currentUserInfo{
+    //ToDo
 }
 
 localStorage.setItem('current_user', JSON.stringify('KastG'));
 localStorage.setItem('posts_number', '0');
 
 function startTests(){
-    const Posts = new postsLent();
+    const Posts = new postsView();
+
+    //const Posts = new postsView(photoPosts);
+   // Posts.removePost('0000001')
+
     Posts.addPost({id: "1",
         description: "Earth view from google number one",
         createdAt: new Date("2019-03-10"),
         location: "Minsk, Belarus",
         author: "KastG",
         photoLink: "images/google-earth-view-1024.jpg",
-        likes: 0});
+        likes: 0
+    });
     Posts.addPost({id: "2",
         description: "Earth view from google number two",
         createdAt: new Date("2019-03-10"),
         location: "Minsk, Belarus",
         author: "Иванов Иван",
         photoLink: "images/google-earth-view-1064.jpg",
-        likes: 2});
+        likes: 2
+    });
     Posts.addPost({id: "3",
         description: "Earth view from google number three",
         createdAt: new Date("2019-03-15"),
         location: "Minsk, Belarus",
         author: "Tim",
         photoLink: "images/google-earth-view-1402.jpg",
-        likes: 0});
+        likes: 0
+    });
     Posts.addPost({id: "4",
         description: "Earth view from google number four",
         createdAt: new Date("2019-03-10"),
         location: "Minsk, Belarus",
         author: "KastG",
         photoLink: "images/google-earth-view-1494.jpg",
-        likes: 0});
+        likes: 0
+    });
+    Posts.addPost({id: "5",
+        description: "Earth view from google number five",
+        createdAt: new Date("2019-03-11"),
+        location: "Belarus",
+        author: "Ivanov Ivan",
+        photoLink: "images/google-earth-view-1868.jpg",
+        likes: 5
+    });
+    Posts.addPost({id: "6",
+        description: "Earth view from google number six",
+        createdAt: new Date("2019-03-15"),
+        location: "Minsk",
+        author: "KastG",
+        photoLink: "images/google-earth-view-2102.jpg",
+        likes: 11
+    });
+    Posts.addPost({id: "7",
+        description: "should be removed",
+        createdAt: new Date("2019-03-11"),
+        location: "Minsk, Belarus",
+        author: "KastG",
+        photoLink: "images/google-earth-view-2154.jpg",
+        likes: 10
+    });
+    Posts.addPost( {id: "8",
+        description: "qaqaqaqaqqaaqaq",
+        createdAt: new Date("2019-03-18"),
+        location: "Minsk, Belarus",
+        author: "KastG",
+        photoLink: "images/google-earth-view-2380.jpg",
+        likes: 20
+    });
+    Posts.addPost({id: "9",
+        description: "Earth view from google number nine",
+        createdAt: new Date("2019-03-12"),
+        location: "Minsk, Belarus",
+        author: "KastG",
+        photoLink: "images/google-earth-view-5023.jpg",
+        likes: 26
+    });
+    Posts.addPost({id: "10",
+        description: "Earth view from google number ten",
+        createdAt: new Date("2019-03-13"),
+        location: "Minsk, Belarus",
+        author: "KastG",
+        photoLink: "images/google-earth-view-5051.jpg",
+        likes: 6
+    });
+
+    Posts.removePost('0000007');
+
+    Posts.editPost('0000008', {description: "I've changed something!!!!"});
 
 }
 
--- UI/scripts/scriptWithPosts.js
+++ UI/scripts/scriptWithPosts.js
@@ -1,163 +1,83 @@
 const photoPosts = [
-    {
-        id: "1",
+    {id: "1",
         description: "Earth view from google number one",
         createdAt: new Date("2019-03-10"),
         location: "Minsk, Belarus",
         author: "KastG",
-        photoLink: "images/google-earth-view-1024.jpg"
+        photoLink: "images/google-earth-view-1024.jpg",
+        likes: 0
     },
-    {
-        id: "2",
+    {id: "2",
         description: "Earth view from google number two",
         createdAt: new Date("2019-03-10"),
         location: "Minsk, Belarus",
         author: "Иванов Иван",
-        photoLink: "images/google-earth-view-1064.jpg"
+        photoLink: "images/google-earth-view-1064.jpg",
+        likes: 2
     },
-    {
-        id: "3",
+    {id: "3",
         description: "Earth view from google number three",
         createdAt: new Date("2019-03-15"),
         location: "Minsk, Belarus",
-        author: "",
-        photoLink: "images/google-earth-view-1402.jpg"
+        author: "Tim",
+        photoLink: "images/google-earth-view-1402.jpg",
+        likes: 0
     },
-    {
-        id: "4",
+    {id: "4",
         description: "Earth view from google number four",
         createdAt: new Date("2019-03-10"),
         location: "Minsk, Belarus",
         author: "KastG",
-        photoLink: "images/google-earth-view-1494.jpg"
+        photoLink: "images/google-earth-view-1494.jpg",
+        likes: 0
     },
-    {
-        id: "5",
+    {id: "5",
         description: "Earth view from google number five",
         createdAt: new Date("2019-03-11"),
         location: "Belarus",
         author: "Ivanov Ivan",
-        photoLink: "images/google-earth-view-1868.jpg"
+        photoLink: "images/google-earth-view-1868.jpg",
+        likes: 5
     },
-    {
-        id: "6",
+    {id: "6",
         description: "Earth view from google number six",
         createdAt: new Date("2019-03-15"),
         location: "Minsk",
         author: "KastG",
-        photoLink: "images/google-earth-view-2102.jpg"
+        photoLink: "images/google-earth-view-2102.jpg",
+        likes: 11
     },
-    {
-        id: "7",
-        description: "Earth view from google number seven",
+    {id: "7",
+        description: "should be removed",
         createdAt: new Date("2019-03-11"),
         location: "Minsk, Belarus",
         author: "KastG",
-        photoLink: "images/google-earth-view-2154.jpg"
+        photoLink: "images/google-earth-view-2154.jpg",
+        likes: 10
     },
-    {
-        id: "8",
-        description: "Earth view from google number eight",
+    {id: "8",
+        description: "qaqaqaqaqqaaqaq",
         createdAt: new Date("2019-03-18"),
         location: "Minsk, Belarus",
         author: "KastG",
-        photoLink: "images/google-earth-view-2380.jpg"
+        photoLink: "images/google-earth-view-2380.jpg",
+        likes: 20
     },
-    {
-        id: "9",
+    {id: "9",
         description: "Earth view from google number nine",
         createdAt: new Date("2019-03-12"),
         location: "Minsk, Belarus",
         author: "KastG",
-        photoLink: "images/google-earth-view-5023.jpg"
+        photoLink: "images/google-earth-view-5023.jpg",
+        likes: 26
     },
-    {
-        id: "10",
+    {id: "10",
         description: "Earth view from google number ten",
         createdAt: new Date("2019-03-13"),
         location: "Minsk, Belarus",
         author: "KastG",
-        photoLink: "images/google-earth-view-5051.jpg"
-    },
-    {
-        id: "11",
-        description: "Фото из Лошицкого парка номер 1",
-        createdAt: new Date("2019-03-10"),
-        location: "Minsk, Belarus",
-        author: "KastG",
-        photoLink: "images/01.jpg"
-    },
-    {
-        id: "12",
-        description: "Фото из Лошицкого парка номер 2",
-        createdAt: new Date("2019-03-9"),
-        location: "Minsk, Belarus",
-        author: "KastG",
-        photoLink: "images/02.jpg"
-    },
-    {
-        id: "13",
-        description: "Фото из Лошицкого парка номер 3",
-        createdAt: new Date("2019-03-10"),
-        location: "Minsk, Belarus",
-        author: "KastG",
-        photoLink: "images/03.jpg"
-    },
-    {
-        id: "14",
-        description: "Фото из Лошицкого парка номер 4",
-        createdAt: new Date("2019-03-10"),
-        location: "Minsk, Belarus",
-        author: "KastG",
-        photoLink: "images/04.jpg"
-    },
-    {
-        id: "15",
-        description: "Фото из Лошицкого парка номер 5",
-        createdAt: new Date("2019-03-18"),
-        location: "Minsk, Belarus",
-        author: "KastG",
-        photoLink: "images/05.jpg"
-    },
-    {
-        id: "16",
-        description: "Фото из Лошицкого парка номер 6",
-        createdAt: new Date("2019-03-19"),
-        location: "Minsk, Belarus",
-        author: "Ivan",
-        photoLink: "images/06.jpg"
-    },
-    {
-        id: "17",
-        description: "Фото из Лошицкого парка номер 7",
-        createdAt: new Date("2019-03-14"),
-        location: "Minsk, Belarus",
-        author: "KastG",
-        photoLink: "images/07.jpg"
-    },
-    {
-        id: "18",
-        description: "Фото из Лошицкого парка номер 8",
-        createdAt: new Date("2019-04-12"),
-        location: "Minsk, Belarus",
-        author: "Ivan",
-        photoLink: "images/08.jpg"
-    },
-    {
-        id: "19",
-        description: "Фото из Лошицкого парка номер 9",
-        createdAt: new Date("2019-04-10"),
-        location: "Minsk, Belarus",
-        author: "KastG",
-        photoLink: "images/09.jpg"
-    },
-    {
-        id: "20",
-        description: "Фото из Лошицкого парка номер 10",
-        createdAt: new Date("2019-04-10"),
-        location: "Minsk, Belarus",
-        author: "KastG",
-        photoLink: "images/10.jpg"
+        photoLink: "images/google-earth-view-5051.jpg",
+        likes: 6
     }
 ];
 
